8b142036b719042dc28f2c45efb3c7fc6bb70f9d
Gabriel <127807744+gabriel-dev1@users.noreply.github.com>
2023-12-13 22:03:39 -0300

    Upload de arquivos

---
+++ product/viewsets/product_viewset.py
@@ -0,0 +1,13 @@
+#  from rest_framework import status 
+#  from rest_framwork.mixins import CreateModelMixin
+#  from rest_framework.response import Response 
+from rest_framework.viewsets import ModelViewSet 
+
+from product.models import Product 
+from product.serializers import ProductSerializer
+
+
+class ProductViewSet(ModelViewSet): 
+    serializer_class = ProductSerializer
+
+    queryset = Product.objects.all()
