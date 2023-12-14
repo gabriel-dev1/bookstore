9046e45015dcee1327c0961e1aa4f38580dab183
Gabriel <127807744+gabriel-dev1@users.noreply.github.com>
2023-12-14 18:41:36 -0300

    Update de arquivos

--- product/viewsets/product_viewset.py
+++ product/viewsets/product_viewset.py
@@ -2,12 +2,10 @@
 #  from rest_framwork.mixins import CreateModelMixin
 #  from rest_framework.response import Response 
 from rest_framework.viewsets import ModelViewSet 
-
 from product.models import Product 
 from product.serializers import ProductSerializer
 
 
-class ProductViewSet(ModelViewSet): 
+class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer
-
-    queryset = Product.objects.all()
+    queryset = Product.objects.all().order_by('id')
