46b1996906fb8bb11ca1348a994185b266d0a505
Gabriel <127807744+gabriel-dev1@users.noreply.github.com>
2023-12-14 18:37:56 -0300

    Update de arquivos

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'order',
     'product',
+    'rest_framework.authtoken',
     'rest_framework',
     'django_extensions',
     'debug_toolbar',
@@ -131,6 +132,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
 }
 
 INTERNAL_IPS = [
--- bookstore/urls.py
+++ bookstore/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from rest_framework.authtoken.views import obtain_auth_token
 from django.urls import path, re_path, include
 import debug_toolbar
 
@@ -24,4 +25,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     re_path('bookstore/(?P<version>(v1|v2))/', include('order.urls')),
     re_path('bookstore/(?P<version>(v1|v2))/', include('product.urls')),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth')
 ]
