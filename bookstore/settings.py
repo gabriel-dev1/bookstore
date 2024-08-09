71a37f2a188ed93c6ca0b7f571aa42fd02de97dd
Gabriel <127807744+gabriel-dev1@users.noreply.github.com>
2024-08-09 10:37:09 -0300

    Upload

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -149,4 +149,4 @@ INTERNAL_IPS = [
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'key')
 DEBUG = int(os.environ.get("DEBUG", default=0))
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '').split(" ")
+#ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '').split(" ")
